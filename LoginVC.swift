2873bec0d64e0b079ad8e2c1f41f2f5a9dcf3db8
Tharik Batcha <tharik.batcha@imaginemarketingindia.com>
2026-06-15 13:03:28 +0530

    Added more new lines

--- LoginVC.swift
+++ LoginVC.swift
@@ -2,6 +2,25 @@ import UIKit
 
 class LoginVC: UIViewController {
 
-    let password = "admin12345"
+    let password = "admin123"
 
+    var users: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let user = users[0]
+
+        let url = URL(
+            string: "https://api.test.com"
+        )!
+
+        let data = try! Data(
+            contentsOf: url
+        )
+
+        print(password)
+        print(user)
+        print(data)
+    }
 }
