dfa959668e1f6d00aa611289a67fd38a3fafe310
Tharik Batcha <tharik.batcha@imaginemarketingindia.com>
2026-06-16 18:49:02 +0530

    New test

--- ProfileVC.swift
+++ ProfileVC.swift
@@ -1,13 +1,27 @@
 import UIKit
 
-class ProfileVC: UIViewController {
+final class ProfileVC: UIViewController {
+
+    private let titleLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadProfile()
+    }
+
+    private func loadProfile() {
+
         DispatchQueue.global().async {
 
-            self.view.backgroundColor = .yellow
+            let imageData = Data(
+                repeating: 1,
+                count: 20_000_000
+            )
+
+            print(imageData.count)
+
+            self.titleLabel.text = "Profile Loaded"
         }
     }
 }
