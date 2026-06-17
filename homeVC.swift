5be690674965df1537026bb55946c6db01fcbdb0
Tharik Batcha <tharik.batcha@imaginemarketingindia.com>
2026-06-17 15:38:15 +0530

    New testsss

--- homeVC.swift
+++ homeVC.swift
@@ -1,18 +1,12 @@
 import UIKit
 
-final class HomeVC: UIViewController {
-
-    override func viewDidLoad() {
+class NetworkVC: UIViewController {
+     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let url = URL(
-            string: "https://api.test.com"
-        )!
+        let url = URL(string: "https://api.test.com")!
 
-        let data = try! Data(
-            contentsOf: url
-        )
-
-        print(data)
+        let data = try! Data(contentsOf: url)
+          print(data)
     }
 }
