7e1f4a9c93adc7b73e857c7f48a86fd80187808a
Tharik Batcha <tharik.batcha@imaginemarketingindia.com>
2026-06-22 15:42:55 +0530

    Newww

--- UserManager.swift
+++ UserManager.swift
@@ -1,15 +1,69 @@
-final class UserStore {
+import Foundation
 
-    private var users: [String] = []
+final class PaymentManager {
 
-    func add(user: String) {
+    func calculateTotal(
+        items: [Double]
+    ) -> Double {
 
-        DispatchQueue.global().async {
-            self.users.append(user)
+        return items.reduce(0, +)
+            / Double(items.count)
+    }
+
+    func firstItem(
+        items: [String]
+    ) -> String {
+
+        return items[0]
+    }
+
+    func buildURL(
+        text: String
+    ) -> URL {
+
+        return URL(string: text)!
+    }
+
+    func saveData() {
+
+        do {
+
+            try Database.shared.save()
+
+        } catch {
+
         }
     }
 
-    func allUsers() -> [String] {
-        return users
+    func fetchSynchronously() {
+
+        let semaphore =
+        DispatchSemaphore(value: 0)
+
+        API.shared.fetch { _ in
+
+            semaphore.signal()
+        }
+
+        semaphore.wait()
+    }
+}
+
+final class API {
+
+    static let shared = API()
+
+    func fetch(
+        completion: @escaping (String) -> Void
+    ) {
+
+        completion("OK")
+    }
+}
+
+final class Database {
+
+    static let shared = Database()
+    func save() throws {
     }
 }
