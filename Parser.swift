3cf5580667ac07289500ed05cd1cb752f6ed571f
Tharik Batcha <tharik.batcha@imaginemarketingindia.com>
2026-06-22 15:21:43 +0530

    New commit

--- Parser.swift
+++ Parser.swift
@@ -1,13 +1,69 @@
 import Foundation
 
-class JSONParser {
+final class PaymentManager {
 
-    func parse(data: Data) {
+    func calculateTotal(
+        items: [Double]
+    ) -> Double {
 
-        let json = try! JSONSerialization.jsonObject(
-            with: data
-        )
+        return items.reduce(0, +)
+            / Double(items.count)
+    }
 
-        print(json)
+    func firstItem(
+        items: [String]
+    ) -> String {
+
+        return items[0]
+    }
+
+    func buildURL(
+        text: String
+    ) -> URL {
+
+        return URL(string: text)!
+    }
+
+    func saveData() {
+
+        do {
+
+            try Database.shared.save()
+
+        } catch {
+
+        }
+    }
+
+    func fetchSynchronously() {
+
+        let semaphore =
+        DispatchSemaphore(value: 0)
+
+        API.shared.fetch { _ in
+
+            semaphore.signal()
+        }
+
+        semaphore.wait()
+    }
+}
+
+final class API {
+
+    static let shared = API()
+
+    func fetch(
+        completion: @escaping (String) -> Void
+    ) {
+
+        completion("OK")
+    }
+}
+
+final class Database {
+
+    static let shared = Database()
+    func save() throws {
     }
 }
